c55ecd1c356994d51eeccf2382bb621393954315
itaypf <67192735+itaypf@users.noreply.github.com>
2021-01-22 12:42:50 +0200

    updated files
    added scoredboard updating with scores of players, added denial of host rolling again before all players finished with the last letter

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { rejects } = require('assert');
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 var express=require('express');
 var fs=require('fs');
 var app=express();
@@ -38,10 +39,12 @@ io.sockets.on('connection',function(socket){
         var room={};
         room.players={};
         room.sockets=[];
+        room.status={};
         room.id=data.input;
         room.categories=data.categories
         socket.nickname=data.name
-        room.players[socket.nickname]="0";
+        room.players[socket.nickname]=0;
+        room.status[socket.nickname]=1;
         room.sockets.push(socket);
         rooms[place]=room;
         place++;
@@ -49,16 +52,19 @@ io.sockets.on('connection',function(socket){
         var clientsList = io.sockets.adapter.rooms;
         console.log(clientsList);
         socket.emit("created",{
+            status:room.status,
             num:0
         });
     });
-    socket.on("joinRoom",function(data){
+    socket.on("joinRoom",function(data){//when a player joins update the player list for all players
+        //and inform them that a player joined
         id=data.input;
         socket.nickname=data.name;
         for (i=0;i<place;i++){
             if(id===rooms[i].id){
                 socket.join(id);
-                rooms[i].players[socket.nickname]="0";
+                rooms[i].players[socket.nickname]=0;
+                rooms[i].status[socket.nickname]=1;
                 rooms[i].sockets.push(socket);
                 console.log(rooms[i].sockets);
                 for(j=0;j<rooms[i].sockets.length;j++)
@@ -66,6 +72,7 @@ io.sockets.on('connection',function(socket){
                     if(rooms[i].sockets[j].id!=socket.id){
                         rooms[i].sockets[j].emit("playerJoined",{
                             players:rooms[i].players,
+                            status:rooms[i].status,
                             name:socket.nickname
                         });
                     }
@@ -102,6 +109,7 @@ io.sockets.on('connection',function(socket){
         {
             if(rooms[i].id===data.room)
             {
+                rooms[i].status=data.status;
                 for(j=0;j<rooms[i].sockets.length;j++)
                 {
                     if(rooms[i].sockets[j].nickname!==socket.nickname)
@@ -126,7 +134,25 @@ io.sockets.on('connection',function(socket){
             });
        }
     });
-    socket.on('disconnect',function(reason){
+    socket.on("update",function(data){
+        for(i=0;i<place;i++)
+        {
+            if(rooms[i].id==data.id)
+            {
+                rooms[i].status[data.name]=1;
+                rooms[i].players[socket.nickname]+=data.num
+                for(j=0;j<rooms[i].sockets.length;j++){
+                    rooms[i].sockets[j].emit("scoreboard",{
+                        players:rooms[i].players,
+                        status:rooms[i].status
+                    });
+                }
+            }
+        }
+    });
+       
+    socket.on('disconnect',function(reason){//when player leaves update the player list for everyone
+        //and inform them that a player left
         console.log('a user disconnected beacuse '+reason);
         for(i=0;i<place;i++){
             for(j=0;j<rooms[i].sockets.length;j++)
@@ -134,6 +160,7 @@ io.sockets.on('connection',function(socket){
                 if(rooms[i].sockets[j].id==socket.id){
                     var nickname=socket.nickname;
                     delete rooms[i].players[socket.nickname];
+                    delete rooms[i].status[socket.nickname];
                     rooms[i].sockets.pop(socket);
                     emitDisconnection(nickname,rooms[i]);
                     break;
@@ -147,7 +174,8 @@ function emitDisconnection(name,room){
     for(i=0;i<room.sockets.length;i++){
         room.sockets[i].emit("playerLeft",{
             name:name,
-            players:room.players
+            players:room.players,
+            status:room.status
         });
     }
 }
